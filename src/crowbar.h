2fad6beb75f0da2e3b244621292de6ad5786abb3
Zeyuan Shang <zeyuanxy@gmail.com>
2014-09-28 14:30:42 +0800

    define basic struct like function, statment, etc in crowbar.h

--- src/crowbar.h
+++ src/crowbar.h
@@ -1,57 +1,283 @@
 #ifndef PRIVATE_CROWBAR_H_INCLUDED
 #define PRIVATE_CROWBAR_H_INCLUDED
+#include <stdio.h>
+#include "MEM.h"
+#include "CRB.h"
+#include "CRB_dev.h"
 
-// Variable
-typedef struct Variable_tag {
-	char	*name;
-	CRB_Value	value;
-	struct 	Variable_tag	*next;
-} Variable;
+#define smaller(a, b) ((a) < (b) ? (a) : (b))
+#define larger(a, b) ((a) > (b) ? (a) : (b))
 
-// ParameterList
-typedef struct ParameterList_tag {
-	char	*name;
-	struct 	ParameterList_tag	*next;
-} ParameterList;
-
-// StatementList
-typedef struct StatementList_tag {
-    Statement*	statement;
-    struct 	StatementList_tag	*next;
-} StatementList;
-
-// Block
-typedef struct {
-	StatementList 	*statement_list;
-} Block;
+#define MESSAGE_ARGUMENT_MAX    (256)
+#define LINE_BUF_SIZE           (1024)
 
 typedef enum {
-	CROWBAR_FUNCTION_DEFINITION = 1,
-	NATIVE_FUNCTION_DEFINITION
+    PARSE_ERR = 1,
+    CHARACTER_INVALID_ERR,
+    FUNCTION_MULTIPLE_DEFINE_ERR,
+    COMPILE_ERROR_COUNT_PLUS_1
+} CompileError;
+
+typedef enum {
+    VARIABLE_NOT_FOUND_ERR = 1,
+    FUNCTION_NOT_FOUND_ERR,
+    ARGUMENT_TOO_MANY_ERR,
+    ARGUMENT_TOO_FEW_ERR,
+    NOT_BOOLEAN_TYPE_ERR,
+    MINUS_OPERAND_TYPE_ERR,
+    BAD_OPERAND_TYPE_ERR,
+    NOT_BOOLEAN_OPERATOR_ERR,
+    FOPEN_ARGUMENT_TYPE_ERR,
+    FCLOSE_ARGUMENT_TYPE_ERR,
+    FGETS_ARGUMENT_TYPE_ERR,
+    FPUTS_ARGUMENT_TYPE_ERR,
+    NOT_NULL_OPERATOR_ERR,
+    DIVISION_BY_ZERO_ERR,
+    GLOBAL_VARIABLE_NOT_FOUND_ERR,
+    GLOBAL_STATEMENT_IN_TOPLEVEL_ERR,
+    BAD_OPERATOR_FOR_STRING_ERR,
+    RUNTIME_ERROR_COUNT_PLUS_1
+} RuntimeError;
+
+typedef enum {
+    INT_MESSAGE_ARGUMENT = 1,
+    DOUBLE_MESSAGE_ARGUMENT,
+    STRING_MESSAGE_ARGUMENT,
+    CHARACTER_MESSAGE_ARGUMENT,
+    POINTER_MESSAGE_ARGUMENT,
+    MESSAGE_ARGUMENT_END
+} MessageArgumentType;
+
+typedef struct {
+    char *format;
+} MessageFormat;
+
+typedef struct Expression_tag Expression;
+
+typedef enum {
+    BOOLEAN_EXPRESSION = 1,
+    INT_EXPRESSION,
+    DOUBLE_EXPRESSION,
+    STRING_EXPRESSION,
+    IDENTIFIER_EXPRESSION,
+    ASSIGN_EXPRESSION,
+    ADD_EXPRESSION,
+    SUB_EXPRESSION,
+    MUL_EXPRESSION,
+    DIV_EXPRESSION,
+    MOD_EXPRESSION,
+    EQ_EXPRESSION,
+    NE_EXPRESSION,
+    GT_EXPRESSION,
+    GE_EXPRESSION,
+    LT_EXPRESSION,
+    LE_EXPRESSION,
+    LOGICAL_AND_EXPRESSION,
+    LOGICAL_OR_EXPRESSION,
+    MINUS_EXPRESSION,
+    FUNCTION_CALL_EXPRESSION,
+    NULL_EXPRESSION,
+    EXPRESSION_TYPE_COUNT_PLUS_1
+} ExpressionType;
+
+#define dkc_is_math_operator(operator) \
+  ((operator) == ADD_EXPRESSION || (operator) == SUB_EXPRESSION\
+   || (operator) == MUL_EXPRESSION || (operator) == DIV_EXPRESSION\
+   || (operator) == MOD_EXPRESSION)
+
+#define dkc_is_compare_operator(operator) \
+  ((operator) == EQ_EXPRESSION || (operator) == NE_EXPRESSION\
+   || (operator) == GT_EXPRESSION || (operator) == GE_EXPRESSION\
+   || (operator) == LT_EXPRESSION || (operator) == LE_EXPRESSION)
+
+#define dkc_is_logical_operator(operator) \
+  ((operator) == LOGICAL_AND_EXPRESSION || (operator) == LOGICAL_OR_EXPRESSION)
+
+typedef struct ArgumentList_tag {
+    Expression *expression;
+    struct ArgumentList_tag *next;
+} ArgumentList;
+
+typedef struct {
+    char        *variable;
+    Expression  *operand;
+} AssignExpression;
+
+typedef struct {
+    Expression  *left;
+    Expression  *right;
+} BinaryExpression;
+
+typedef struct {
+    char                *identifier;
+    ArgumentList        *argument;
+} FunctionCallExpression;
+
+struct Expression_tag {
+    ExpressionType type;
+    int line_number;
+    union {
+        CRB_Boolean             boolean_value;
+        int                     int_value;
+        double                  double_value;
+        char                    *string_value;
+        char                    *identifier;
+        AssignExpression        assign_expression;
+        BinaryExpression        binary_expression;
+        Expression              *minus_expression;
+        FunctionCallExpression  function_call_expression;
+    } u;
+};
+
+typedef struct Statement_tag Statement;
+
+typedef struct StatementList_tag {
+    Statement   *statement;
+    struct StatementList_tag    *next;
+} StatementList;
+
+typedef struct {
+    StatementList       *statement_list;
+} Block;
+
+typedef struct IdentifierList_tag {
+    char        *name;
+    struct IdentifierList_tag   *next;
+} IdentifierList;
+
+typedef struct {
+    IdentifierList      *identifier_list;
+} GlobalStatement;
+
+typedef struct Elsif_tag {
+    Expression  *condition;
+    Block       *block;
+    struct Elsif_tag    *next;
+} Elsif;
+
+typedef struct {
+    Expression  *condition;
+    Block       *then_block;
+    Elsif       *elsif_list;
+    Block       *else_block;
+} IfStatement;
+
+typedef struct {
+    Expression  *condition;
+    Block       *block;
+} WhileStatement;
+
+typedef struct {
+    Expression  *init;
+    Expression  *condition;
+    Expression  *post;
+    Block       *block;
+} ForStatement;
+
+typedef struct {
+    Expression *return_value;
+} ReturnStatement;
+
+typedef enum {
+    EXPRESSION_STATEMENT = 1,
+    GLOBAL_STATEMENT,
+    IF_STATEMENT,
+    WHILE_STATEMENT,
+    FOR_STATEMENT,
+    RETURN_STATEMENT,
+    BREAK_STATEMENT,
+    CONTINUE_STATEMENT,
+    STATEMENT_TYPE_COUNT_PLUS_1
+} StatementType;
+
+struct Statement_tag {
+    StatementType       type;
+    int                 line_number;
+    union {
+        Expression      *expression_s;
+        GlobalStatement global_s;
+        IfStatement     if_s;
+        WhileStatement  while_s;
+        ForStatement    for_s;
+        ReturnStatement return_s;
+    } u;
+};
+
+typedef struct ParameterList_tag {
+    char        *name;
+    struct ParameterList_tag *next;
+} ParameterList;
+
+typedef enum {
+    CROWBAR_FUNCTION_DEFINITION = 1,
+    NATIVE_FUNCTION_DEFINITION
 } FunctionDefinitionType;
 
 typedef struct FunctionDefinition_tag {
-	char 	*name;
-	FunctionDefinitionType 	type;
-	union {
-		struct {
-			ParameterList 	*parameter;
-			Block 	*block;
-		} crowbar_f;
-		struct {
-			CRB_NativeFunctionProc 	*proc;
-		} native_f;
-	} u;
-	struct FunctionDefinition_tag 	*next;
+    char                *name;
+    FunctionDefinitionType      type;
+    union {
+        struct {
+            ParameterList       *parameter;
+            Block               *block;
+        } crowbar_f;
+        struct {
+            CRB_NativeFunctionProc      *proc;
+        } native_f;
+    } u;
+    struct FunctionDefinition_tag       *next;
 } FunctionDefinition;
 
-struct CRB_Interpreter_tag {
-	MEM_Storage 	interpreter_storage;
-	MEM_Storage 	execute_storage;
-	Variable 	*variable;
-	FunctionDefinition 	*function_list;
-	StatementList 	*statement_list;
-	int 	current_line_number;
+typedef struct Variable_tag {
+    char        *name;
+    CRB_Value   value;
+    struct Variable_tag *next;
+} Variable;
+
+typedef enum {
+    NORMAL_STATEMENT_RESULT = 1,
+    RETURN_STATEMENT_RESULT,
+    BREAK_STATEMENT_RESULT,
+    CONTINUE_STATEMENT_RESULT,
+    STATEMENT_RESULT_TYPE_COUNT_PLUS_1
+} StatementResultType;
+
+typedef struct {
+    StatementResultType type;
+    union {
+        CRB_Value       return_value;
+    } u;
+} StatementResult;
+
+typedef struct GlobalVariableRef_tag {
+    Variable    *variable;
+    struct GlobalVariableRef_tag *next;
+} GlobalVariableRef;
+
+typedef struct {
+    Variable    *variable;
+    GlobalVariableRef   *global_variable;
+} LocalEnvironment;
+
+struct CRB_String_tag {
+    int         ref_count;
+    char        *string;
+    CRB_Boolean is_literal;
 };
 
-#endif
+typedef struct {
+    CRB_String  *strings;
+} StringPool;
+
+struct CRB_Interpreter_tag {
+    MEM_Storage         interpreter_storage;
+    MEM_Storage         execute_storage;
+    Variable            *variable;
+    FunctionDefinition  *function_list;
+    StatementList       *statement_list;
+    int                 current_line_number;
+};
+
+
+
+#endif /* PRIVATE_CROWBAR_H_INCLUDED */
