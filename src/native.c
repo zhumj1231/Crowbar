c4707b15420e696b604975bc4d2b13d723462c46
Zeyuan Shang <zeyuanxy@gmail.com>
2014-10-02 18:31:58 +0800

    add fgets and fputs in native.c

--- src/native.c
+++ src/native.c
@@ -85,3 +85,94 @@ CRB_Value crb_nv_fclose_proc(CRB_Interpreter *interpreter,
 
     return value;
 }
+
+CRB_Value crb_nv_fgets_proc(CRB_Interpreter *interpreter,
+                            int arg_count, CRB_Value *args)
+{
+    CRB_Value value;
+    FILE *fp;
+    char buf[LINE_BUF_SIZE];
+    char *ret_buf = NULL;
+    int ret_len = 0;
+
+    if (arg_count < 1) {
+        crb_runtime_error(0, ARGUMENT_TOO_FEW_ERR,
+                          MESSAGE_ARGUMENT_END);
+    } else if (arg_count > 1) {
+        crb_runtime_error(0, ARGUMENT_TOO_MANY_ERR,
+                          MESSAGE_ARGUMENT_END);
+    }
+    if (args[0].type != CRB_NATIVE_POINTER_VALUE
+        || !check_native_pointer(&args[0])) {
+        crb_runtime_error(0, FGETS_ARGUMENT_TYPE_ERR,
+                          MESSAGE_ARGUMENT_END);
+    }
+    fp = args[0].u.native_pointer.pointer;
+
+    while (fgets(buf, LINE_BUF_SIZE, fp)) {
+        int new_len;
+        new_len = ret_len + strlen(buf);
+        ret_buf = MEM_realloc(ret_buf, new_len + 1);
+        if (ret_len == 0) {
+            strcpy(ret_buf, buf);
+        } else {
+            strcat(ret_buf, buf);
+        }
+        ret_len = new_len;
+        if (ret_buf[ret_len-1] == '\n')
+            break;
+    }
+    if (ret_len > 0) {
+        value.type = CRB_STRING_VALUE;
+        value.u.string_value = crb_create_crowbar_string(interpreter, ret_buf);
+    } else {
+        value.type = CRB_NULL_VALUE;
+    }
+
+    return value;
+}
+
+CRB_Value crb_nv_fputs_proc(CRB_Interpreter *interpreter,
+                            int arg_count, CRB_Value *args)
+{
+    CRB_Value value;
+    FILE *fp;
+
+    value.type = CRB_NULL_VALUE;
+    if (arg_count < 2) {
+        crb_runtime_error(0, ARGUMENT_TOO_FEW_ERR,
+                          MESSAGE_ARGUMENT_END);
+    } else if (arg_count > 2) {
+        crb_runtime_error(0, ARGUMENT_TOO_MANY_ERR,
+                          MESSAGE_ARGUMENT_END);
+    }
+    if (args[0].type != CRB_STRING_VALUE
+        || (args[1].type != CRB_NATIVE_POINTER_VALUE
+            || !check_native_pointer(&args[1]))) {
+        crb_runtime_error(0, FPUTS_ARGUMENT_TYPE_ERR,
+                          MESSAGE_ARGUMENT_END);
+    }
+    fp = args[1].u.native_pointer.pointer;
+
+    fputs(args[0].u.string_value->string, fp);
+
+    return value;
+}
+
+void
+crb_add_std_fp(CRB_Interpreter *inter)
+{
+    CRB_Value fp_value;
+
+    fp_value.type = CRB_NATIVE_POINTER_VALUE;
+    fp_value.u.native_pointer.info = &st_native_lib_info;
+
+    fp_value.u.native_pointer.pointer = stdin;
+    CRB_add_global_variable(inter, "STDIN", &fp_value);
+
+    fp_value.u.native_pointer.pointer = stdout;
+    CRB_add_global_variable(inter, "STDOUT", &fp_value);
+
+    fp_value.u.native_pointer.pointer = stderr;
+    CRB_add_global_variable(inter, "STDERR", &fp_value);
+}
