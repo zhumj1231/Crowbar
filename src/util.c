f392857e103bb0292b99988df89984c0ab02115c
Zeyuan Shang <zeyuanxy@gmail.com>
2014-10-02 15:13:07 +0800

    add add_local_variable and add_global_variable in util.c

--- src/util.c
+++ src/util.c
@@ -75,3 +75,30 @@ crb_execute_malloc(CRB_Interpreter *inter, size_t size)
 
     return p;
 }
+
+void
+crb_add_local_variable(LocalEnvironment *env,
+                       char *identifier, CRB_Value *value)
+{
+    Variable    *new_variable;
+
+    new_variable = MEM_malloc(sizeof(Variable));
+    new_variable->name = identifier;
+    new_variable->value = *value;
+    new_variable->next = env->variable;
+    env->variable = new_variable;
+}
+
+void
+CRB_add_global_variable(CRB_Interpreter *inter, 
+						char *identifier, CRB_Value *value)
+{
+    Variable    *new_variable;
+
+    new_variable = crb_execute_malloc(inter, sizeof(Variable));
+    new_variable->name = crb_execute_malloc(inter, strlen(identifier) + 1);
+    strcpy(new_variable->name, identifier);
+    new_variable->next = inter->variable;
+    inter->variable = new_variable;
+    new_variable->value = *value;
+}
