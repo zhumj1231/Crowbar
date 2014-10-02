fbfc3efa028e1d0e31de432f9c1541d199aa7e58
Zeyuan Shang <zeyuanxy@gmail.com>
2014-10-02 12:43:43 +0800

    add yyerror to throw parse error

--- src/error.c
+++ src/error.c
@@ -233,3 +233,19 @@ crb_runtime_error(int line_number, RuntimeError id, ...)
     exit(1);
 }
 
+int
+yyerror(char const *str)
+{
+    char *near_token;
+
+    if (yytext[0] == '\0') {
+        near_token = "EOF";
+    } else {
+        near_token = yytext;
+    }
+    crb_compile_error(PARSE_ERR,
+                      STRING_MESSAGE_ARGUMENT, "token", near_token,
+                      MESSAGE_ARGUMENT_END);
+
+    return 0;
+}
