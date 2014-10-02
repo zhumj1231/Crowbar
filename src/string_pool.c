e6df63716f31806910488961e835ced7ca3cb03e
Zeyuan Shang <zeyuanxy@gmail.com>
2014-10-02 14:54:10 +0800

    add reference count in string_pool.c

--- src/string_pool.c
+++ src/string_pool.c
@@ -16,3 +16,44 @@ alloc_crb_string(CRB_Interpreter *inter, char *str, CRB_Boolean is_literal)
 
     return ret;
 }
+
+CRB_String *
+crb_literal_to_crb_string(CRB_Interpreter *inter, char *str)
+{
+    CRB_String *ret;
+
+    ret = alloc_crb_string(inter, str, CRB_TRUE);
+    ret->ref_count = 1;
+
+    return ret;
+}
+
+void
+crb_refer_string(CRB_String *str)
+{
+    str->ref_count++;
+}
+
+void
+crb_release_string(CRB_String *str)
+{
+    str->ref_count--;
+
+    DBG_assert(str->ref_count >= 0, ("str->ref_count..%d\n",
+                                     str->ref_count));
+    if (str->ref_count == 0) {
+        if (!str->is_literal) {
+            MEM_free(str->string);
+        }
+        MEM_free(str);
+    }
+}
+
+CRB_String *
+crb_create_crowbar_string(CRB_Interpreter *inter, char *str)
+{
+    CRB_String *ret = alloc_crb_string(inter, str, CRB_FALSE);
+    ret->ref_count = 1;
+
+    return ret;
+}
