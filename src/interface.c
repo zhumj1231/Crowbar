0f6849df647ec95dd03220bbd0fe4b5507dd43ac
Zeyuan Shang <zeyuanxy@gmail.com>
2014-10-02 18:49:45 +0800

    add interpreting interfaces in interface.c

--- src/interface.c
+++ src/interface.c
@@ -13,6 +13,76 @@ add_native_functions(CRB_Interpreter *inter)
     CRB_add_native_function(inter, "fputs", crb_nv_fputs_proc);
 }
 
+CRB_Interpreter *
+CRB_create_interpreter(void)
+{
+    MEM_Storage storage;
+    CRB_Interpreter *interpreter;
+
+    storage = MEM_open_storage(0);
+    interpreter = MEM_storage_malloc(storage,
+                                     sizeof(struct CRB_Interpreter_tag));
+    interpreter->interpreter_storage = storage;
+    interpreter->execute_storage = NULL;
+    interpreter->variable = NULL;
+    interpreter->function_list = NULL;
+    interpreter->statement_list = NULL;
+    interpreter->current_line_number = 1;
+
+    crb_set_current_interpreter(interpreter);
+    add_native_functions(interpreter);
+
+    return interpreter;
+}
+
+void
+CRB_compile(CRB_Interpreter *interpreter, FILE *fp)
+{
+    extern int yyparse(void);
+    extern FILE *yyin;
+
+    crb_set_current_interpreter(interpreter);
+
+    yyin = fp;
+    if (yyparse()) {
+        /* BUGBUG */
+        fprintf(stderr, "Error ! Error ! Error !\n");
+        exit(1);
+    }
+    crb_reset_string_literal_buffer();
+}
+
+void
+CRB_interpret(CRB_Interpreter *interpreter)
+{
+    interpreter->execute_storage = MEM_open_storage(0);
+    crb_add_std_fp(interpreter);
+    crb_execute_statement_list(interpreter, NULL, interpreter->statement_list);
+}
+
+static void
+release_global_strings(CRB_Interpreter *interpreter) {
+    while (interpreter->variable) {
+        Variable *temp = interpreter->variable;
+        interpreter->variable = temp->next;
+        if (temp->value.type == CRB_STRING_VALUE) {
+            crb_release_string(temp->value.u.string_value);
+        }
+    }
+}
+
+void
+CRB_dispose_interpreter(CRB_Interpreter *interpreter)
+{
+    release_global_strings(interpreter);
+
+    if (interpreter->execute_storage) {
+        MEM_dispose_storage(interpreter->execute_storage);
+    }
+
+    MEM_dispose_storage(interpreter->interpreter_storage);
+}
+
 void
 CRB_add_native_function(CRB_Interpreter *interpreter,
                         char *name, CRB_NativeFunctionProc *proc)
