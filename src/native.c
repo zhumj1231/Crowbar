5b01f355f2f27cc566801f2d1d35b21e900513d8
Zeyuan Shang <zeyuanxy@gmail.com>
2014-10-02 17:48:36 +0800

    add native.c to define native functions

---
+++ src/native.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+#include "MEM.h"
+#include "DBG.h"
+#include "CRB_dev.h"
+#include "crowbar.h"
+
+#define NATIVE_LIB_NAME "crowbar.lang.file"
+
+static CRB_NativePointerInfo st_native_lib_info = {
+    NATIVE_LIB_NAME
+};
